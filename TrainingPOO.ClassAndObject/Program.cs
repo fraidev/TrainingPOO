069b0f876386898a56b6654cb67931c00154582b
fraifelipe <fraifelipe@gmail.com>
2018-07-19 23:33:32 -0300

    Csprojs

---
+++ TrainingPOO.ClassAndObject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrainingPOO.ClassAndObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TrainingPOO.CompositionAndAggregation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrainingPOO.CompositionAndAggregation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
